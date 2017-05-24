312b31b98aadcf075847a95cca7781c8be49e508
Lev Grossman <levgrossman@Levs-MacBook-Pro-2.local>
2017-05-24 11:31:58 -0400

    edits

--- MATLAB/reconstruct.m
+++ MATLAB/reconstruct.m
@@ -1,8 +1,26 @@
-function [O_rec] = reconstruct(W, O_p)
+function [] = reconstruct(W, O_p)
+    % show partial object
+    vs = [0.5, 0.5, 0.5];
+    voxel_image(to_v(O_p), vs);
+    view([-37.5, 30]);
+    
+    clc;
+    disp('Press any key to see reconstructed object')
+    pause;
+        
+    % create reconstructed object
     V = binselect(O_p);
     A = (W') * (V') * V * W;
     b = (W') * (V') * (V * O_p);
     O_rec = W * (A \ b);
+    
+    % show reconstructed object
+    voxel_image(to_v(O_rec), vs);
+    view([-37.5, 30]);
+    
+    pause; clc;
+    
+    
 end
 
 function V = binselect (vox)
@@ -11,6 +29,27 @@ function V = binselect (vox)
     V = V(~isnan(vox), :);
 end
 
+
+function [pts] = to_v(data)
+
+data = reshape(data, [100, 100, 100]); %resize to 100,100,100 matrix
+
+[M,N,Z] = size(data);
+pts = [];
+
+for i = 1:M
+    for j = 1:N
+        for k = 1:Z
+            if data(i,j,k) > 0.1
+                %fprintf('%d %d %d\n', i,j,k);
+                tmp = [i j k];
+                pts = [pts; tmp];
+            end
+        end
+    end
+end
+
+end
 
 
 
