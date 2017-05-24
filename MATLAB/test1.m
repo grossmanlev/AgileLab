01ed889783c61164c50d3da8567c4fdc86b69631
Lev Grossman <levgrossman@Levs-MacBook-Pro-2.local>
2017-05-24 13:49:17 -0400

    writeup

--- MATLAB/test1.m
+++ MATLAB/test1.m
@@ -4,4 +4,9 @@ opts = struct( 'maxiters', 5,...
                'algorithm', 'vb',...
                'minangle', 0 );
            
-[ A, S, Mu, V, cv, hp, lc ] = pca_full( W', ncomp, opts );
+[ A, S, Mu, V, cv, hp, lc ] = pca_full( data', ncomp, opts );
+
+
+% get the data in a 100000 x 5 matrix
+% then data (data == 0) = NaN;
+% feed data' to the pca_full, with ncomp 5, and then S' is the basis!
