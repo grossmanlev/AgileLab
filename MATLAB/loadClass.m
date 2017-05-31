e23ab4738629fbcf6d2c0a663a8763e8a15745e6
grossmanlev <grossmanlev@gmail.com>
2017-05-31 17:02:52 -0400

    added meshes

--- MATLAB/loadClass.m
+++ MATLAB/loadClass.m
@@ -6,7 +6,7 @@
 % is_basis - true  : loading a full object class
 %          - false : loading object
 
-function[W] = loadClass(path, is_occ, is_basis)
+function[W] = loadClass(path, is_occ, is_basis, size)
 original_dir = pwd;
 % For now, assume he voxels will be stored in 100x100x100
 
@@ -14,7 +14,7 @@ original_dir = pwd;
 cd (path);
 F = dir ('*.txt');
 
-for f = 1:size(F,1)
+for f = 1:numel(F)
     %Open file
     fileID = fopen(F(f).name, 'r');
     
@@ -26,13 +26,13 @@ for f = 1:size(F,1)
     Vdata = A;
 
     %Create 3D voxel object matrix
-    V = zeros([100 100 100]);
+    V = zeros([size size size]);
     for i = Vdata'
         tmp = i';
         if(is_occ && tmp(1) > 0)
-            V(tmp(1)+50,tmp(2)+50,tmp(3)+50) = NaN; % or NaN
+            V(tmp(1)+(size/2),tmp(2)+(size/2),tmp(3)+(size/2)) = NaN; % or NaN
         else
-            V(tmp(1)+50,tmp(2)+50,tmp(3)+50) = 1; 
+            V(tmp(1)+(size/2),tmp(2)+(size/2),tmp(3)+(size/2)) = 1; 
         end
     end;
     
